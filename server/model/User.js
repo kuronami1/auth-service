1a17717eec7833ec2e3c7d7bd401d39fac9d4054
kuronami1 <vladmasliev777@gmail.com>
2026-04-08 12:34:01 +0300

    add userSchema for db

---
+++ server/model/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

a87afc2549263bc321b6a0f590f87ef8659e998a
kuronami1 <vladmasliev777@gmail.com>
2026-04-08 14:32:08 +0300

    add login logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../model/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
   const {
@@ -19,6 +20,38 @@ const registerUser = async (req, res) => {
   await User.create({ name: name, email: email, password: hashedPassword });
 
   return res.status(201).json({ name: name, email: email });
+}
+
+const loginUser = async (req, res) => {
+  const {
+    email,
+    password
+  } = req.body;
+
+  const user = await User.findOne({ email: email })
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const verifyPassword = await bcrypt.compare(password, user.password)
+  if (!verifyPassword) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const exp = Number(process.env.TOKEN_TTL) * 60 * 1000;
+  const token = jwt.sign(
+    {
+      sub: user._id,
+      username: user.name,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: exp,
+    }
+  )
+
+  return res.status(200).cookie("token", token, { httpOnly: true, maxAge: exp });
 }
 
 export default registerUser;

89154a9409ab4213c10d37555f2848a5dc21d020
kuronami1 <vladmasliev777@gmail.com>
2026-04-09 16:11:28 +0300

    minor fix for RegisterBtn

--- client/src/components/RegisterBtn.jsx
+++ client/src/components/RegisterBtn.jsx
@@ -1,7 +1,8 @@
-export default function RegisterBtn() {
+export default function RegisterBtn({ children = "Register" }) {
   return (
-    <>
-      <button type="submit" className="w-32 h-12 transtion duration-300 text-white text-xl font-semibold bg-sky-500/100 border-none rounded-xl shadow-md cursor-pointer hover:bg-sky-400 hover:scale-110 active:bg-sky-300">Register</button>
-    </>
+    <button type="submit" className="w-32 h-12 transition-all  duration-300 text-white text-xl font-semibold bg-sky-500 
+        border-none rounded-xl shadow-md cursor-pointer hover:bg-sky-400 hover:scale-110 active:bg-sky-300">
+      {children}
+    </button>
   )
 }

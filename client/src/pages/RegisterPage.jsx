37a0951389e5f400f7e5d4090fbae0a555bf1df8
kuronami1 <vladmasliev777@gmail.com>
2026-04-09 17:01:50 +0300

    start building register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,22 +3,25 @@ import RegisterBtn from "../components/RegisterBtn";
 export default function ResgisterPage() {
   return (
     <>
-      <h1>Register</h1>
-      <form method="post">
-        <label>
-          <span>Username</span>
-          <input type="text" id="name" name="username" required />
-        </label>
-        <label>
-          <span>Email</span>
-          <input type="email" id="email" name="email" required />
-        </label>
-        <label>
-          <span>Password</span>
-          <input type="password" id="password" name="password" required />
-        </label>
-        <RegisterBtn />
-      </form>
+      <div className="min-h-screen flex justify-center items-center">
+        <form className="flex flex-col gap-4 w-80 p-8 roundend-xl shadow-md">
+          <h1 className="text-2xl font-bold mb-8">Register</h1>
+          <label className="flex flex-col gap-1">
+            <span className="text-md text-gray-600 font-bold">Username</span>
+            <input type="text" className="px-2 py-1 border-indigo-300 border-2 rounded-md transition-colors duration-300 focus:border-indigo-500 outline-none" id="name" name="username" required />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>Email</span>
+            <input type="email" className="px-2 py-1 border-indigo-300 border-2 rounded-md transition-colors duration-300 focus:border-indigo-500 outline-none" id="email" name="email" required />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>Password</span>
+            <input type="password" className="px-2 py-1 border-indigo-300 border-2 rounded-md transition-colors duration-300 focus:border-indigo-500 outline-none" id="password" name="password" required />
+          </label>
+          <RegisterBtn />
+        </form>
+
+      </div>
     </>
   )
 

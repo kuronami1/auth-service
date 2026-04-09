fc68d25be0c57676e6c09c57e24d93d6b87a5c8b
kuronami1 <vladmasliev777@gmail.com>
2026-04-09 16:03:12 +0300

    fix for tailwind + add registerBtn

---
+++ client/src/components/RegisterBtn.jsx
@@ -0,0 +1,7 @@
+export default function RegisterBtn() {
+  return (
+    <>
+      <button type="submit" className="w-32 h-12 transtion duration-300 text-white text-xl font-semibold bg-sky-500/100 border-none rounded-xl shadow-md cursor-pointer hover:bg-sky-400 hover:scale-110 active:bg-sky-300">Register</button>
+    </>
+  )
+}
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,25 @@
-export default function ResgisterPage() {
-  <>
+import RegisterBtn from "../components/RegisterBtn";
 
-  </>
+export default function ResgisterPage() {
+  return (
+    <>
+      <h1>Register</h1>
+      <form method="post">
+        <label>
+          <span>Username</span>
+          <input type="text" id="name" name="username" required />
+        </label>
+        <label>
+          <span>Email</span>
+          <input type="email" id="email" name="email" required />
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="password" id="password" name="password" required />
+        </label>
+        <RegisterBtn />
+      </form>
+    </>
+  )
+
 }
